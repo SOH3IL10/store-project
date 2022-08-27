19889101d8f88d08e75c086d5f706caccc6b5be3
SOH3IL <soheil.e.001@gmail.com>
2022-08-27 17:06:34 +0430

    handle close search resault on click outside box

--- src/Components/Nav/Components/SearchBox.jsx
+++ src/Components/Nav/Components/SearchBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useTransition } from 'react';
+import React, { useRef, useState, useTransition } from 'react';
 import { useEffect } from 'react';
 import '../style.scss';
 import SearchIcon from '@mui/icons-material/Search';
@@ -7,10 +7,12 @@ import SearchResultItems from './SearchResultItems';
 import ClearIcon from '@mui/icons-material/Clear';
 import Grid from '@mui/material/Grid';
 
-export default function SearchBox({theme}) {
+export default function SearchBox({ theme }) {
     const [value, setValue] = useState('');
     const [products, setProducts] = useState([]);
     const [filteredData, setFilteredData] = useState([]);
+    const inputRef = useRef(null);
+    const searchBoxRef = useRef(null);
 
     const [isPending, startTransition] = useTransition();
 
@@ -28,31 +30,40 @@ export default function SearchBox({theme}) {
             setFilteredData([]);
         else {
             startTransition(() => {
-                const newFilterd = products.filter(product => product.title.toLowerCase().includes(value.toLowerCase()) || product.description.toLowerCase().includes(value.toLowerCase()))
+                const newFilterd = products.filter(product => product.title.toLowerCase().includes(value.toLowerCase()))
                 setFilteredData(newFilterd);
             })
-
         }
     }
 
+    useEffect(() => {
+        function handleCloseSearchBox(e) {
+            if ((inputRef.current && !inputRef.current.contains(e.target)) && (searchBoxRef.current && !searchBoxRef.current.contains(e.target)))
+                setValue('');
+        }
+
+        document.body.addEventListener('click', handleCloseSearchBox);
+
+        return () => document.body.removeEventListener('click', handleCloseSearchBox);
+    }, [])
+
     return (
         <>
-            <Grid item xs={7} md={9} lg={8} className={theme=== 'dark' ? 'backgroundDark navSearch' : 'navSearch'}>
-                <input type="text" placeholder='Search products & trands' value={value} onChange={(e) => setValue(e.target.value) } />
+            <Grid item xs={7} md={9} lg={8} ref={inputRef} className={theme === 'dark' ? 'backgroundDark navSearch' : 'navSearch'}>
+                <input type="text" placeholder='Search products & trands' value={value} onChange={(e) => setValue(e.target.value)} />
                 <div className='navSearchIcon'>
                     {value.length > 0 ? <ClearIcon onClick={() => setValue('')} /> : <SearchIcon />}
                 </div>
             </Grid>
             {
                 filteredData.length > 0 &&
-                <div className={theme === 'dark' ? 'backgroundDark boxShadowDark searchResult' : 'searchResult'}>
+                <div ref={searchBoxRef} className={theme === 'dark' ? 'backgroundDark boxShadowDark searchResult' : 'searchResult'}>
+                    <div className='searchWrapper'></div>
                     {
                         filteredData.map(item => <SearchResultItems key={item.id} resultData={item} />)
                     }
                 </div>
-
             }
-
         </>
     )
 }
